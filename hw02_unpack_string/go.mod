06af4873171252c72bb5c95e97cb98d4cd1a8c5d
Roman Gafurov <gafurov.roma89@gmail.com>
2021-03-11 11:38:34 +0300

    HW2 is completed (#5)

--- hw02_unpack_string/go.mod
+++ hw02_unpack_string/go.mod
@@ -1,4 +1,4 @@
-module github.com/fixme_my_friend/hw02_unpack_string
+module github.com/seth2810/otus_homework/hw02_unpack_string
 
 go 1.16
 
--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,72 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+const escapeChar = '\\'
+
+func Unpack(input string) (string, error) {
+	if input == "" {
+		return input, nil
+	}
+
+	var prevChar, char rune
+	var builder strings.Builder
+	var isEscaped, isPrevDigit, isDigit bool
+
+	runes := []rune(input)
+	char = runes[0]
+	isDigit = unicode.IsDigit(char)
+
+	if isDigit {
+		return "", ErrInvalidString
+	}
+
+	for i := 1; i < len(runes); i++ {
+		prevChar = char
+		isPrevDigit = isDigit
+
+		char = runes[i]
+		isDigit = unicode.IsDigit(char)
+
+		// handle escapes only for digits and slashes
+		if prevChar == escapeChar && !isEscaped {
+			if !isDigit && char != escapeChar {
+				return "", ErrInvalidString
+			}
+
+			isEscaped = true
+			continue
+		}
+
+		// handle digit before current char
+		if isPrevDigit && !isEscaped {
+			if isDigit {
+				return "", ErrInvalidString
+			}
+
+			continue
+		}
+
+		if isDigit {
+			count, _ := strconv.Atoi(string(char))
+			builder.WriteString(strings.Repeat(string(prevChar), count))
+		} else {
+			builder.WriteRune(prevChar)
+		}
+
+		isEscaped = false
+	}
+
+	if isEscaped || !unicode.IsDigit(char) {
+		builder.WriteRune(char)
+	}
+
+	// Place your code here
+	return builder.String(), nil
 }
