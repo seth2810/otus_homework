77ad55cb7102dc2638ab0618929d24dbb8f8377f
Roman Gafurov <gafurov.roma89@gmail.com>
2021-03-22 11:04:50 +0300

    HW4 is completed (#7)

--- hw04_lru_cache/cache.go
+++ hw04_lru_cache/cache.go
@@ -1,5 +1,9 @@
 package hw04lrucache
 
+import (
+	"sync"
+)
+
 type Key string
 
 type Cache interface {
@@ -9,8 +13,7 @@ type Cache interface {
 }
 
 type lruCache struct {
-	Cache // Remove me after realization.
-
+	sync.Mutex
 	capacity int
 	queue    List
 	items    map[Key]*ListItem
@@ -21,10 +24,67 @@ type cacheItem struct {
 	value interface{}
 }
 
-func NewCache(capacity int) Cache {
-	return &lruCache{
-		capacity: capacity,
-		queue:    NewList(),
-		items:    make(map[Key]*ListItem, capacity),
+func (c *lruCache) Set(key Key, value interface{}) bool {
+	c.Lock()
+
+	defer c.Unlock()
+
+	item, exist := c.items[key]
+
+	// I don't really understand why we use cacheItem type
+	cachedValue := &cacheItem{key: string(key), value: value}
+
+	if exist {
+		item.Value = cachedValue
+		c.queue.MoveToFront(item)
+		return true
 	}
+
+	c.queue.PushFront(cachedValue)
+	c.items[key] = c.queue.Front()
+
+	if c.queue.Len() > c.capacity {
+		back := c.queue.Back()
+		c.queue.Remove(back)
+		delete(c.items, Key(back.Value.(*cacheItem).key))
+	}
+
+	return false
+}
+
+func (c *lruCache) Get(key Key) (interface{}, bool) {
+	c.Lock()
+
+	defer c.Unlock()
+
+	item, exist := c.items[key]
+
+	if !exist {
+		return nil, false
+	}
+
+	c.queue.MoveToFront(item)
+
+	cachedItem := item.Value.(*cacheItem)
+
+	return cachedItem.value, true
+}
+
+func (c *lruCache) Clear() {
+	c.Lock()
+
+	defer c.Unlock()
+
+	c.queue = NewList()
+	c.items = make(map[Key]*ListItem, c.capacity)
+}
+
+func NewCache(capacity int) Cache {
+	c := &lruCache{
+		capacity: capacity,
+	}
+
+	c.Clear()
+
+	return c
 }
