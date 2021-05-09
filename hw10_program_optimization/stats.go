822807f7d50ef5c84f823f8cb76b98c93d936e06
Roman Gafurov <gafurov.roma89@gmail.com>
2021-05-09 15:24:55 +0300

    HW10 is completed (#13)

--- hw10_program_optimization/stats.go
+++ hw10_program_optimization/stats.go
@@ -1,67 +1,41 @@
 package hw10programoptimization
 
 import (
-	"encoding/json"
+	"bufio"
 	"fmt"
 	"io"
-	"io/ioutil"
-	"regexp"
 	"strings"
-)
 
-type User struct {
-	ID       int
-	Name     string
-	Username string
-	Email    string
-	Phone    string
-	Password string
-	Address  string
-}
+	"github.com/valyala/fastjson"
+)
 
 type DomainStat map[string]int
 
 func GetDomainStat(r io.Reader, domain string) (DomainStat, error) {
-	u, err := getUsers(r)
-	if err != nil {
-		return nil, fmt.Errorf("get users error: %w", err)
-	}
-	return countDomains(u, domain)
-}
+	domainSuffix := fmt.Sprintf(".%s", domain)
 
-type users [100_000]User
-
-func getUsers(r io.Reader) (result users, err error) {
-	content, err := ioutil.ReadAll(r)
-	if err != nil {
-		return
-	}
-
-	lines := strings.Split(string(content), "\n")
-	for i, line := range lines {
-		var user User
-		if err = json.Unmarshal([]byte(line), &user); err != nil {
-			return
-		}
-		result[i] = user
-	}
-	return
-}
-
-func countDomains(u users, domain string) (DomainStat, error) {
 	result := make(DomainStat)
 
-	for _, user := range u {
-		matched, err := regexp.Match("\\."+domain, []byte(user.Email))
-		if err != nil {
+	s := bufio.NewScanner(r)
+	s.Split(bufio.ScanLines)
+
+	for s.Scan() {
+		if len(s.Bytes()) == 0 {
+			continue
+		}
+
+		if err := fastjson.ValidateBytes(s.Bytes()); err != nil {
 			return nil, err
 		}
 
-		if matched {
-			num := result[strings.ToLower(strings.SplitN(user.Email, "@", 2)[1])]
-			num++
-			result[strings.ToLower(strings.SplitN(user.Email, "@", 2)[1])] = num
+		email := fastjson.GetString(s.Bytes(), "Email")
+
+		if strings.HasSuffix(email, domainSuffix) {
+			if emailParts := strings.SplitN(email, "@", 2); len(emailParts) > 1 {
+				result[strings.ToLower(emailParts[1])]++
+			}
 		}
 	}
+
 	return result, nil
 }
