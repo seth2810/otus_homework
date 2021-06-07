3027fd4137cfa6fc87fc8515f32036157900f860
Roman Gafurov <gafurov.roma89@gmail.com>
2021-06-07 13:58:46 +0300

    HW11 is completed (#14)

--- hw11_telnet_client/telnet.go
+++ hw11_telnet_client/telnet.go
@@ -1,8 +1,15 @@
 package main
 
 import (
+	"errors"
 	"io"
+	"net"
 	"time"
+)
+
+var (
+	ErrNoSuchHost     = errors.New("no such host")
+	ErrConnectTimeout = errors.New("connect timeout")
 )
 
 type TelnetClient interface {
@@ -13,9 +20,47 @@ type TelnetClient interface {
 }
 
 func NewTelnetClient(address string, timeout time.Duration, in io.ReadCloser, out io.Writer) TelnetClient {
-	// Place your code here.
-	return nil
+	return &client{address, timeout, in, out, nil}
 }
 
-// Place your code here.
-// P.S. Author's solution takes no more than 50 lines.
+type client struct {
+	address string
+	timeout time.Duration
+	in      io.ReadCloser
+	out     io.Writer
+	conn    io.ReadWriteCloser
+}
+
+func (c *client) Connect() (err error) {
+	c.conn, err = net.DialTimeout("tcp", c.address, c.timeout)
+
+	var dnsErr *net.DNSError
+
+	if errors.As(err, &dnsErr) && dnsErr.IsNotFound {
+		err = ErrNoSuchHost
+	}
+
+	var netErr net.Error
+
+	if errors.As(err, &netErr) && netErr.Timeout() {
+		err = ErrConnectTimeout
+	}
+
+	return
+}
+
+func (c *client) Send() error {
+	_, err := io.Copy(c.conn, c.in)
+
+	return err
+}
+
+func (c *client) Receive() error {
+	_, err := io.Copy(c.out, c.conn)
+
+	return err
+}
+
+func (c *client) Close() error {
+	return c.conn.Close()
+}
